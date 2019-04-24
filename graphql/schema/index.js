d530e4df73398dfad7a4181f167a2dd5d16916f6
J <socept@gmail.com>
2019-04-24 08:32:54 -0400

    Refactoring resolvers and adding the login feature

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,56 +1,55 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-            creator: User!
-        }
-
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-            createdEvents: [Event!]
-        }
-
-        type Booking {
-            id: ID!
-            user: User!
-            event: Event!
-            createdAt: String!
-            updatedAt: String!
-        }
-
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input UserInput {
-            email: String!
-            password: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-            bookings: [Booking!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-            bookEvent(eventId: ID!): Booking!
-            cancelBooking(bookingId: ID!): Event!
-        }
-
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `)
+  type Booking {
+      _id: ID!
+      event: Event!
+      user: User!
+      createdAt: String!
+      updatedAt: String!
+  }
+  type Event {
+    _id: ID!
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+    creator: User!
+  }
+  type User {
+    _id: ID!
+    email: String!
+    password: String
+    createdEvents: [Event!]
+  }
+  input EventInput {
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
+  input UserInput {
+    email: String!
+    password: String!
+  }
+  type AuthData {
+    _id: ID!
+    token: String!
+    tokenExpiration: Int!
+  }
+  type RootQuery {
+      events: [Event!]!
+      bookings: [Booking!]!
+      login(email: String!, password: String!): AuthData!
+  }
+  type RootMutation {
+      createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
+      bookEvent(eventId: ID!): Booking!
+      cancelBooking(bookingId: ID!): Event!
+  }
+  schema {
+      query: RootQuery
+      mutation: RootMutation
+  }
+`);
